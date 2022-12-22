5721a4ff68cd21cd18ef9bbd2cf287b71207f027
EvaVozz <voznukeeva@ukr.net>
2022-12-22 10:05:54 +0200

    adding

--- src/App.js
+++ src/App.js
@@ -23,7 +23,9 @@ class App extends Component {
 			{ id: 'id-2', text: 'Todo-2', compledted: true},
 			{ id: 'id-3', text: 'Todo-3', compledted: false },
 			{ id: 'id-4', text: 'Todo-4', compledted: true }
-		],
+      ],
+    name: "Привет я Реакт", // value инпут
+    password: ''
 	}
 
 	deleteToDo = todoId => { // создаем методкласса для удаления туду по Айди
@@ -32,16 +34,62 @@ class App extends Component {
         return todo.id !== todoId
       })
 		}))
-	}
+  }
+
+
+
+  //События инпут - получения данных из инпута! можно написать отдельно для каждого инпута метод
+  //но это не удобно поскольку ипутов может быть много, поэтому лучше написать один метод для все ипутов
+  // через атрибут name=""
+
+  // hendleNameChangeInput = (event) => { // передаем событие ипут
+  //   console.log(event.currentTarget.value)
+  //   this.setState({
+  //     name: event.currentTarget.value
+  //   })
+  // }
+
+  //  hendlePasswordChangeInput = (event) => { // передаем событие ипут
+  //   console.log(event.currentTarget.value)
+  //   this.setState({
+  //     password: event.currentTarget.value
+  //   })
+  // }
+
+  hendleOnChange = (event) => {
+    console.log(event.currentTarget);
+    console.log(event.currentTarget.name); // input name or input passwor
+    console.log(event.currentTarget.value); // result 
+    this.setState({
+      [event.currentTarget.name]: event.currentTarget.value
+    })
+  }
+  //Метод Сабмит отправка формы
+  hendleSubmit = (event) => {
+    event.preventDefault()
+    console.log(this.state);
+  }
 
 	render() {
-    const { todolist } = this.state // destructurisation
+    const { todolist } = this.state // destructurisation 
     const completedToDo = todolist.reduce((total, totdo) => {
       return totdo.compledted ? total + 1 : total
     }, 0)
     console.log(completedToDo);
 		return (
-	<>
+      <>
+        <form onSubmit={this.hendleSubmit}>
+          <label>
+            Name
+           <input type="text" name="name" value={this.state.name} onChange={this.hendleOnChange}></input>
+          </label>
+            <label>
+            Password
+           <input type="numbers" name="password" value={this.state.password} onChange={this.hendleOnChange}></input>
+          </label>
+          <button type='submit'>Отправить</button>
+        </form>
+       
       <h1>Состояние компонента</h1>
       {/* <Counter initialValue={10} /> 
       <CounterTow initialValue={10} />
